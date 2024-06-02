cb6155796817b806e21f6f19eb5f661cac067ac4
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2024-06-02 22:43:24 +0100

    :)

--- savings_growth.php
+++ savings_growth.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 
-<table border="1" cellspacing="0" width="450" style="text-align:center">
+<table border="1" cellspacing="0" cellpadding="5" width="450" style="text-align:center; margin: 20px 0 0 20px">
     <tr>
         <th>Age</th>
         <th>Account Balance</th>
